88e96a2a19265da8487c6923f500cb6eda7346fa
metraf <firsov.tyoma@gmail.com>
2018-11-18 20:36:04 +0100

    fixed insert

--- 3_insert_company.down.sql
+++ 3_insert_company.down.sql
@@ -1,1 +1,1 @@
-DELETE FROM company WHERE id = -1;
+DELETE FROM company WHERE id = 1;
--- 3_insert_company.up.sql
+++ 3_insert_company.up.sql
@@ -1,2 +1,1 @@
-INSERT INTO company (id, gv_id, name, symbol, gv_url, parsed_docs) VALUES (-1, -1, 'test', 'TEST', 'https://sec.gov/-1', FALSE);
-
+INSERT INTO company (gv_id, name, symbol, gv_url, parsed_docs) VALUES ('0001047340', 'FRESH DEL MONTE PRODUCE INC', 'FDP', 'https://www.sec.gov/Archives/edgar/data/1047340', FALSE);
